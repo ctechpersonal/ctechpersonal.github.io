00eaf3b1dc374932fbfe3ed6903e9d9f72e207ef
n-pryjmak <pryjmak97@gmail.com>
2022-05-02 15:58:32 +0300

    Initial commit

---
+++ assets/js/desktop.js
@@ -0,0 +1,47 @@
+"use strict";
+
+(function() {
+
+
+
+
+
+
+
+// let navBox = document.querySelector('.primary-nav-box');
+let nav = document.querySelector('.nav');
+let homeLink = nav.firstElementChild;
+
+
+
+window.on('scroll', function() {
+
+   if ( window.scrollY >  210 && homeLink.classList.contains('d-hidden') ) {
+      homeLink.classList.remove('d-hidden');
+      nav.classList.remove('margin-top');
+
+      // navBox.classList.toggle('scrolled');
+
+      console.log('HOME link has been ADDED to navigation');
+   }
+   else
+   if ( window.scrollY <= 210 && !homeLink.classList.contains('d-hidden') ) {
+      homeLink.classList.add('d-hidden');
+      nav.classList.add('margin-top');
+
+      // navBox.classList.toggle('scrolled');
+      
+      console.log('HOME link has been REMOVED from navigation');
+   }
+
+});
+
+
+
+
+
+// desctopMedia.addListener(e => {
+//    if(e.matches) { document.getElementById('aside').style.minHeight = document.body.offsetHeight + 'px';}
+// });
+
+}());
---
+++ assets/js/mobile.js
@@ -0,0 +1,50 @@
+"use strict";
+
+(function() {
+
+
+
+
+
+
+
+let navBox = document.querySelector('.primary-nav-box');
+let burger = document.querySelector('.burger');
+
+
+// closing Primary Navigation
+document.body.on('click', (e) => {
+   if (!navBox.classList.contains('m-hidden') && !e.target.closest('.nav') && !e.opening)
+      navBox.classList.add('m-hidden');
+});
+
+window.on('scroll', () => {
+   if (!navBox.classList.contains('m-hidden') && window.scrollY > 100)
+      navBox.classList.add('m-hidden');
+});
+
+document.on('keyup', (e) => {
+   if (!navBox.classList.contains('m-hidden') && e.code == "Escape")
+      navBox.classList.add('m-hidden');
+});
+
+
+// opening Primary Navigation
+burger.on('click', (e) => {
+   if (navBox.classList.contains('m-hidden')) {
+      navBox.classList.remove('m-hidden');
+      e.opening = true;
+   }
+});
+
+
+
+
+
+
+
+// window.matchMedia( '(max-width: 768px)' ).addListener(e => { // // maxWidth768
+//    if(e.matches) { document.getElementById('aside').style.minHeight = 75 + 'px';}
+// });
+
+}());
